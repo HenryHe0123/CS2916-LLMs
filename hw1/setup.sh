de341cf06140402230f36e3ca75781dcb7e3bfc5
Henry He <119676880+HenryHe0123@users.noreply.github.com>
2024-03-25 16:55:11 +0800

    feat llama.py

--- hw1/setup.sh
+++ hw1/setup.sh
@@ -5,7 +5,7 @@
 # For OSX, use `pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0`
 # For Linux and Windows (GPU version), use `pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cu118`
 # For Linux and Windows (CPU version), use `pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cpu`
-pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cpu
 pip install tqdm==4.66.1
 pip install requests==2.31.0
 pip install importlib-metadata==3.7.0
